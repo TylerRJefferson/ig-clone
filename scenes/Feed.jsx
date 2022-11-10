97f791d7b7d2449ebbbc1c6d7d717c01ca70429f
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-10 09:34:01 -0500

    added style sheet and began cleaning up default native code

---
+++ scenes/Feed.jsx
@@ -0,0 +1,11 @@
+import { ScrollView } from "react-native";
+import styles from "../styles";
+
+export default function Feed() {
+  return (
+    <ScrollView style={styles.feed}>
+
+    </ScrollView>
+  );
+};
+
---
+++ styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  feed: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+});
+
+export default styles;
